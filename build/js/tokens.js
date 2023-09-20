10dd79a9546066b1845d36cff51e74fa029e257c
joaomarcos <jjmarcos18@hotmail.com>
2023-09-20 12:33:52 -0300

    new test

--- build/js/tokens.js
+++
@@ -1,8 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Wed, 20 Sep 2023 14:38:30 GMT
- */
-
-module.exports = {
-  "ColorBlack": "#0e0e0e"
-};
---
+++ build/js/variables.js
@@ -0,0 +1,44 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 20 Sep 2023 15:20:53 GMT
+ */
+
+module.exports = {
+  "color": {
+    "black": {
+      "description": "",
+      "type": "color",
+      "value": "#0e0e0e",
+      "blendMode": "normal",
+      "extensions": {
+        "org.lukasoppermann.figmaDesignTokens": {
+          "styleId": "S:cd2ef7320a2b680438d72d2989be0033feb3b66c,",
+          "exportKey": "color"
+        }
+      },
+      "filePath": "input/tokens.json",
+      "isSource": true,
+      "original": {
+        "description": "",
+        "type": "color",
+        "value": "#0e0e0eff",
+        "blendMode": "normal",
+        "extensions": {
+          "org.lukasoppermann.figmaDesignTokens": {
+            "styleId": "S:cd2ef7320a2b680438d72d2989be0033feb3b66c,",
+            "exportKey": "color"
+          }
+        }
+      },
+      "name": "ColorBlack",
+      "attributes": {
+        "category": "color",
+        "type": "black"
+      },
+      "path": [
+        "color",
+        "black"
+      ]
+    }
+  }
+};
